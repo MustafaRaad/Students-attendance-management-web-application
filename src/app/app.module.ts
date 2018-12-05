6f5e765d1fce0fd1963676c1c3e2dd0c8e4c6c3c
MustafaRaad <ofydesigner.2008@gmail.com>
2018-12-06 01:35:48 +0300

    put apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule, Router } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { DepComponent } from "./dep/dep.component";
+import { LoginComponent } from "./login/login.component";
 
+// const routes: Routes = [
+//   { path: "", redirectTo: "home", pathMatch: "full" },
+//   { path: "home", component: HomeComponent },
+//   { path: "dep", component: DepComponent },
+// { path: "admin", component: AdminComponent },
+// { path: "lec", component: LecComponent },
+// ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DepComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
