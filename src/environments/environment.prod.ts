9f111bed81bd05983a1170becde2eea445ea5087
Mustafa Raad <ofydesigner.2008@gmail.com>
2020-03-23 03:08:15 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAMqpxpBbbvt9MgNj6qifvND15izxMV6-E",
+    apiKey: "",
     authDomain: "attendance-uitc.firebaseapp.com",
     databaseURL: "https://attendance-uitc.firebaseio.com",
     projectId: "attendance-uitc",
